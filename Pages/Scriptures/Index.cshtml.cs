cd17ca7a6209763a185e6f624de7409c6963eb4d
RodriguezOswaldo <owner2303@gmail.com>
2019-11-01 11:56:35 -0400

    It doesn't filter yet

--- Pages/Scriptures/Index.cshtml.cs
+++ Pages/Scriptures/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ScriptureJournal.Models;
 
@@ -17,11 +18,24 @@ namespace ScriptureJournal.Pages.Scriptures
         {
             _context = context;
         }
+        [BindProperty(SupportsGet = true)]
+        public IList<Journal> Journal { get; set; }
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Books { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BooksInScriptures { get; set; }
 
-        public IList<Journal> Journal { get;set; }
+        
 
         public async Task OnGetAsync()
         {
+            var books = from m in _context.Journal
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(s => s.Book.Contains(SearchString));
+            }
             Journal = await _context.Journal.ToListAsync();
         }
     }
