1d6696e8adeb63765933f540839f7b6a8e01bdc1
Oswaldo Rodriguez <Owner2303@gmail.com>
2019-10-31 23:07:05 -0400

    Update the pages - Done

--- Models/Journal.cs
+++ Models/Journal.cs
@@ -10,10 +10,20 @@ namespace ScriptureJournal.Models
     {
     
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 1)]
         public string Book { get; set; }
+
+        [StringLength(60, MinimumLength = 1)]
         public string BookInScriptures { get; set; }
+
+        [Range(1, 100), DataType(DataType.Custom)]
         public int Chapter { get; set; }
+
+        [Range(1, 100), DataType(DataType.Custom)]
         public int Verse { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
         public string Comments { get; set; }
 
         [DataType(DataType.Date)]
